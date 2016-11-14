3a0ebf0b40ffefc7682fdd0ae0ba9678646c500f
sicos1977 <sicos2002@hotmail.com>
2016-11-14 18:42:11 +0100

    Upped version number

--- OfficeConverterTestTool/ViewerForm.designer.cs
+++ OfficeConverterTestTool/ViewerForm.designer.cs
@@ -35,10 +35,10 @@
             // 
             // SelectButton
             // 
-            this.SelectButton.Location = new System.Drawing.Point(12, 11);
-            this.SelectButton.Margin = new System.Windows.Forms.Padding(2);
+            this.SelectButton.Location = new System.Drawing.Point(24, 21);
+            this.SelectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SelectButton.Name = "SelectButton";
-            this.SelectButton.Size = new System.Drawing.Size(92, 23);
+            this.SelectButton.Size = new System.Drawing.Size(184, 44);
             this.SelectButton.TabIndex = 6;
             this.SelectButton.Text = "Select file";
             this.SelectButton.UseVisualStyleBackColor = true;
@@ -47,35 +47,36 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 44);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(20, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.Size = new System.Drawing.Size(82, 25);
             this.label1.TabIndex = 8;
             this.label1.Text = "Output:";
             // 
             // OutputTextBox
             // 
-            this.OutputTextBox.Location = new System.Drawing.Point(10, 63);
+            this.OutputTextBox.Location = new System.Drawing.Point(20, 121);
+            this.OutputTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.OutputTextBox.Multiline = true;
             this.OutputTextBox.Name = "OutputTextBox";
-            this.OutputTextBox.Size = new System.Drawing.Size(385, 114);
+            this.OutputTextBox.Size = new System.Drawing.Size(766, 216);
             this.OutputTextBox.TabIndex = 9;
             // 
             // ViewerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 184);
+            this.ClientSize = new System.Drawing.Size(804, 354);
             this.Controls.Add(this.OutputTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.SelectButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "ViewerForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.Text = "OfficeConverter test tool v1.0";
+            this.Text = "OfficeConverter test tool v1.2";
             this.Load += new System.EventHandler(this.ViewerForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
