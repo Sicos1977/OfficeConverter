e6bccfbf3781e8244a3feb68815176851b66d6be
sicos1977 <sicos2002@hotmail.com>
2018-09-28 18:25:42 +0200

    Add service

---
+++ OfficeConverterService/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeConverterService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+#if (DEBUG)
+        #region DebugOnStart
+        /// <summary>
+        /// Starts the service in debugging mode from Visual Studio
+        /// </summary>
+        public void DebugOnStart()
+        {
+            OnStart(null);
+        }
+        #endregion
+#endif
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
