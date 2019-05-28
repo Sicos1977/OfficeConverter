9512de0892a4cfd01b8cecf5fd2abf3c569b669b
Kees <magic-sessions@home.nl>
2019-05-28 13:16:26 +0200

    Upped version number and copyright

--- OfficeConverter/Properties/AssemblyInfo.cs
+++ OfficeConverter/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OfficeConverter")]
-[assembly: AssemblyCopyright("Copyright © 2014 - 2018 Kees van Spelde")]
+[assembly: AssemblyCopyright("Copyright © 2014 - 2019 Kees van Spelde")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.10.0")]
-[assembly: AssemblyFileVersion("1.3.10.0")]
+[assembly: AssemblyVersion("1.3.11.0")]
+[assembly: AssemblyFileVersion("1.3.11.0")]
