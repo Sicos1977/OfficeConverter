ea3e7638088e2288ed33bc00e800d66c0e1610cc
Kees <magic-sessions@home.nl>
2020-04-09 09:30:05 +0200

    Upgrade code

--- OfficeConverter/Helpers/FileManager.cs
+++ OfficeConverter/Helpers/FileManager.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 
 //
 // FileManager.cs
@@ -36,75 +34,143 @@ namespace OfficeConverter.Helpers
     /// </summary>
     internal static class FileManager
     {
-        #region CheckForBackSlash
+        #region Consts
         /// <summary>
-        /// Check if there is a backslash at the end of the string and if not add it
+        /// De maximale pad lengte in Windows
+        /// </summary>
+        private const int MaxPath = 248;
+        #endregion
+
+        #region CheckForDirectorySeparator
+        /// <summary>
+        /// Check if there is a directory separator char at the end of the string and if not add it
         /// </summary>
         /// <param name="line"></param>
         /// <returns></returns>
-        public static string CheckForBackSlash(string line)
+        public static string CheckForDirectorySeparator(string line)
         {
-            if (line.EndsWith("\\"))
+            var separator = Path.DirectorySeparatorChar.ToString();
+
+            if (line.EndsWith(separator))
                 return line;
 
-            return line + "\\";
+            return line + separator;
+        }
+        #endregion
+        
+        #region ValidateLongFileName
+        /// <summary>
+        /// Validates the length of <paramref name="fileName"/>, when this is longer then
+        /// 260 chars it will be truncated.
+        /// zodat deze wel past
+        /// </summary>
+        /// <param name="fileName">The filename with path</param>
+        /// <param name="extraTruncateSize">Optional extra truncate size, when not used the filename is truncated until it fits</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Raised when no path or file name is given in the <paramref name="fileName"/></exception>
+        /// <exception cref="PathTooLongException">Raised when it is not possible to truncate the <paramref name="fileName"/></exception>
+        public static string ValidateLongFileName(string fileName, int extraTruncateSize = -1)
+        {
+            var fileNameWithoutExtension = GetFileNameWithoutExtension(fileName);
+
+            //if (string.IsNullOrWhiteSpace(fileNameWithoutExtension))
+            //    throw new ArgumentException(@"No file name is given, e.g. c:\temp\temp.txt", nameof(fileName));
+
+            var extension = GetExtension(fileName);
+
+            if (string.IsNullOrWhiteSpace(extension))
+                extension = string.Empty;
+
+            var path = GetDirectoryName(fileName);
+
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentException(@"No path is given, e.g. c:\temp\temp.txt", nameof(fileName));
+
+            path = CheckForDirectorySeparator(path);
+
+            if (fileName.Length <= MaxPath)
+                return fileName;
+
+            var maxFileNameLength = MaxPath - path.Length - extension.Length;
+            if (extraTruncateSize != -1)
+                maxFileNameLength -= extraTruncateSize;
+
+            if (maxFileNameLength < 1)
+                throw new PathTooLongException("Unable the truncate the fileName '" + fileName + "', current size '" +
+                                               fileName.Length + "'");
+
+            return path + fileNameWithoutExtension.Substring(0, maxFileNameLength) + extension;
         }
         #endregion
 
-        #region FileExistsMakeNew
+        #region GetExtension
         /// <summary>
-        /// Checks if a file already exists and if so adds a number until the file is unique
+        /// Returns the extension of the specified <paramref name="path"/> string
         /// </summary>
-        /// <param name="fileName">The file to check</param>
+        /// <param name="path">The path of the file</param>
         /// <returns></returns>
-        public static string FileExistsMakeNew(string fileName)
+        /// <exception cref="ArgumentException">Raised when no path is given</exception>
+        public static string GetExtension(string path)
         {
-            var i = 2;
-            var path = CheckForBackSlash(Path.GetDirectoryName(fileName));
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentException("path");
 
-            var tempFileName = fileName;
+            var splittedPath = path.Split(Path.DirectorySeparatorChar);
+            var fileName = splittedPath[splittedPath.Length - 1];
 
-            while (File.Exists(tempFileName))
-            {
-                tempFileName = path + Path.GetFileNameWithoutExtension(fileName) + "_" + i + Path.GetExtension(fileName);
-                i += 1;
-            }
+            var index = fileName.LastIndexOf(".", StringComparison.Ordinal);
 
-            return tempFileName;
+            return index == -1
+                ? string.Empty
+                : fileName.Substring(fileName.LastIndexOf(".", StringComparison.Ordinal), fileName.Length - index);
         }
         #endregion
 
-        #region RemoveInvalidFileNameChars
+        #region GetFileNameWithoutExtension
         /// <summary>
-        /// Removes illegal filename characters
+        /// Returns the file name of the specified <paramref name="path"/> string without the extension
         /// </summary>
-        /// <param name="fileName"></param>
+        /// <param name="path">The path of the file</param>
         /// <returns></returns>
-        public static string RemoveInvalidFileNameChars(string fileName)
+        /// <exception cref="ArgumentException"></exception>
+        public static string GetFileNameWithoutExtension(string path)
         {
-            return Path.GetInvalidFileNameChars().Aggregate(fileName, (current, c) => current.Replace(c.ToString(CultureInfo.InvariantCulture), string.Empty));
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentException(@"No path given", nameof(path));
+
+            var splittedPath = path.Split(Path.DirectorySeparatorChar);
+            var fileName = splittedPath[splittedPath.Length - 1];
+            return !fileName.Contains(".")
+                ? fileName
+                : fileName.Substring(0, fileName.LastIndexOf(".", StringComparison.Ordinal));
         }
         #endregion
 
-        #region GetFileSizeString
+        #region GetDirectoryName
         /// <summary>
-        /// Gives the size of a file in Windows format (GB, MB, KB, Bytes)
+        /// Returns the directory information for the specified <paramref name="path"/> string
         /// </summary>
-        /// <param name="bytes">Filesize in bytes</param>
+        /// <param name="path">The path of a file or directory</param>
         /// <returns></returns>
-        public static string GetFileSizeString(double bytes)
+        public static string GetDirectoryName(string path)
         {
-            var size = "0 Bytes";
-            if (bytes >= 1073741824.0)
-                size = String.Format(CultureInfo.InvariantCulture, "{0:##.##}", bytes / 1073741824.0) + " GB";
-            else if (bytes >= 1048576.0)
-                size = String.Format(CultureInfo.InvariantCulture, "{0:##.##}", bytes / 1048576.0) + " MB";
-            else if (bytes >= 1024.0)
-                size = String.Format(CultureInfo.InvariantCulture, "{0:##.##}", bytes / 1024.0) + " KB";
-            else if (bytes > 0 && bytes < 1024.0)
-                size = bytes + " Bytes";
+            //GetDirectoryName('C:\MyDir\MySubDir\myfile.ext') returns 'C:\MyDir\MySubDir'
+            //GetDirectoryName('C:\MyDir\MySubDir') returns 'C:\MyDir'
+            //GetDirectoryName('C:\MyDir\') returns 'C:\MyDir'
+            //GetDirectoryName('C:\MyDir') returns 'C:\'
+            //GetDirectoryName('C:\') returns ''
 
-            return size;
+            var splittedPath = path.Split(Path.DirectorySeparatorChar);
+
+            if (splittedPath.Length <= 1)
+                return string.Empty;
+
+            var result = splittedPath[0];
+
+            for (var i = 1; i < splittedPath.Length - 1; i++)
+                result += Path.DirectorySeparatorChar + splittedPath[i];
+
+            return result;
         }
         #endregion
     }
